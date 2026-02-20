4a0fa56fda81d5788f2cc28b122760e16c20e3e4
udin <pbo@yes.com>
2026-02-20 07:19:42 +0700

    first commit

---
+++ 1/PBO_1/src/pbo_1/PBO_1.java
@@ -0,0 +1,12 @@
+package pbo_1;
+
+public class PBO_1 {
+    public static void main(String[] args) {
+        String ListOfAsdos[] = {"Mukti","Fajar", "Michael", "Alexander", "Lucas", "Eric", "Thosi"};
+        for (String asdos : ListOfAsdos){
+            System.out.println("Asdos : " + asdos);
+        }
+        
+    }
+    
+}
