3e92e97b7647ff9108e8c566fef24d9278d4076f
farel@ucup19 <alexanderkimf@gmail.com>
2026-02-20 07:51:09 +0700

    commit 2 lewat lazygit
    yeyyy

--- 1/PBO_1/src/pbo_1/PBO_1.java
+++ 1/PBO_1/src/pbo_1/PBO_1.java
@@ -8,6 +8,7 @@ public class PBO_1 {
     }
     System.out.println("WOI INI SUDAH HARUSNYA PULANG, INI DARI GIT, 2, make an edit");
     System.out.println("Sekarang ini aku dari laptop pake nvim");
+    /*Aku ini bikin comment lagi di nvim, commit pake lazygit*/
 
   }
 
