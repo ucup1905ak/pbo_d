c994ce37424c009b8434b933618e71a9f928a46a
farel@ucup19 <alexanderkimf@gmail.com>
2026-02-20 07:48:36 +0700

    ini dari nvim

--- 1/PBO_1/src/pbo_1/PBO_1.java
+++ 1/PBO_1/src/pbo_1/PBO_1.java
@@ -1,13 +1,14 @@
 package pbo_1;
 
 public class PBO_1 {
-    public static void main(String[] args) {
-        String ListOfAsdos[] = {"Mukti","Fajar", "Michael", "Alexander", "Lucas", "Eric", "Thosi"};
-        for (String asdos : ListOfAsdos){
-            System.out.println("Asdos : " + asdos);
-        }
-        System.err.println("WOI INI SUDAH HARUSNYA PULANG, INI DARI GIT, 2, make an edit");
-        
+  public static void main(String[] args) {
+    String ListOfAsdos[] = { "Mukti", "Fajar", "Michael", "Alexander", "Lucas", "Eric", "Thosi" };
+    for (String asdos : ListOfAsdos) {
+      System.out.println("Asdos : " + asdos);
     }
-    
+    System.out.println("WOI INI SUDAH HARUSNYA PULANG, INI DARI GIT, 2, make an edit");
+    System.out.println("Sekarang ini aku dari laptop pake nvim");
+
+  }
+
 }
