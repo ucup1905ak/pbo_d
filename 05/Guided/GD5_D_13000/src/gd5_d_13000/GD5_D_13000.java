4be1a9fe7deca792591fe566a58430916cd772f5
farel@ucup19 <alexanderkimf@gmail.com>
2026-04-16 21:57:09 +0700

    gd5

--- 05/Guided/GD5_D_13000/src/gd5_d_13000/GD5_D_13000.java
+++ 05/Guided/GD5_D_13000/src/gd5_d_13000/GD5_D_13000.java
@@ -1,33 +1,33 @@
 package gd5_d_13000;
 public class GD5_D_13000 {
     public static void main(String[] args) {
-    GenericClass<String> genericStr = new GenericClass<String>("nama praktikan"); //diubah
-    System.out.println(genericStr.getValue()); //nama praktikan
+    GenericClass<String> genericStr = new GenericClass<String>("Farelino Alexander Kim"); 
+    System.out.println(genericStr.getValue()); 
 
     GenericClass<Double> genericDbl = new GenericClass<Double>(3.14);
-    System.out.println(genericDbl.getValue()); //3.14
-
+    System.out.println(genericDbl.getValue()); 
+    
     TwoPair<Integer, Integer> twoInt = new TwoPair<Integer, Integer>(1, 2);
-    System.out.println(twoInt.getValueK()); // 1
-    System.out.println(twoInt.getValueV()); // 2
+    System.out.println(twoInt.getValueK()); 
+    System.out.println(twoInt.getValueV()); 
 
-    TwoPair<Integer, String> diffType = new TwoPair<>(1, "sinay");
-    System.out.println(diffType.getValueK()); // 1
-    System.out.println(diffType.getValueV()); // sinay
+    TwoPair<Integer, String> diffType = new TwoPair<>(1, "ucup");
+    System.out.println(diffType.getValueK()); 
+    System.out.println(diffType.getValueV()); 
 
     TwoPair<TwoPair<Integer, String>, Double> inside = new TwoPair<>(diffType, 4.00);
-    System.out.println(inside.getValueK().getValueK()); // 1
-    System.out.println(inside.getValueK().getValueV()); // sinay
-    System.out.println(inside.getValueV()); // 4.00
+    System.out.println(inside.getValueK().getValueK()); 
+    System.out.println(inside.getValueK().getValueV()); 
+    System.out.println(inside.getValueV()); 
 
     GenericList<Integer> npm = new GenericList<>();
-    // masukan 5 digit npm
+    
     npm.add(1);
-    npm.add(2);
-    npm.add(2);
-    npm.add(9);
-    npm.add(1);
-    npm.print(); // 12291
+    npm.add(3);
+    npm.add(0);
+    npm.add(0);
+    npm.add(0);
+    npm.print(); // 13000
 
     GenericList<Character> atma = new GenericList<>();
     atma.add('A');
@@ -36,9 +36,9 @@ public class GD5_D_13000 {
     atma.add('A');
     atma.print(); // ATMA
 
-    Mahasigma sinay = new Mahasigma("sinay", "230712291", 4.0);
-    Mahasigma alex = new Mahasigma("alexander wongso", "230712519", 4.0);
-    Mahasigma anda = new Mahasigma("nama praktikan", "npm praktikan", 4.0);
+    Mahasigma sinay = new Mahasigma("Farel", "2407130000", 3.92);
+    Mahasigma alex = new Mahasigma("Rhhexen Thenzie", "240712805", 4.0);
+    Mahasigma anda = new Mahasigma("Silvanus Widyatama", "240712992", 4.0);
 
     GenericList<Mahasigma> mhs = new GenericList<>();
     mhs.add(sinay);
