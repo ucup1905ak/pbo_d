d759bdf85a62898841ed6c4951be7226610012cd
UCUP <alexanderkimf@gmail.com>
2026-06-12 07:12:53 +0700

    ugd mulai

---
+++ 10/UNGUIDED/UGD10_D_13000/src/ugd10_d_13000/UGD10_D_13000.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ugd10_d_13000;
+
+/**
+ *
+ * @author Farelino Alexander Kim - 2140713000
+ */
+public class UGD10_D_13000 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
